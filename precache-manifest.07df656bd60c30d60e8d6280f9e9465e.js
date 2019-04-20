c7f99ee9e2a8461a634889f2c6ba21cf10e2c110
toihocweb <44312672+toihocweb@users.noreply.github.com>
2019-04-20 13:09:53 +0700

    Add files via upload

---
+++ precache-manifest.07df656bd60c30d60e8d6280f9e9465e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db777e168797a1c9961d",
+    "url": "/searchit/static/js/runtime~main.8dcf750f.js"
+  },
+  {
+    "revision": "f7fa2de2ad2b90e086e3",
+    "url": "/searchit/static/js/main.7565e2e5.chunk.js"
+  },
+  {
+    "revision": "27c43ddf2cf90881e399",
+    "url": "/searchit/static/js/2.c59b9399.chunk.js"
+  },
+  {
+    "revision": "f7fa2de2ad2b90e086e3",
+    "url": "/searchit/static/css/main.883d8b25.chunk.css"
+  },
+  {
+    "revision": "80ece25ecf925b35b88ca28340f806bc",
+    "url": "/searchit/index.html"
+  }
+];
